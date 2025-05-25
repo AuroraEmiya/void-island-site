d123f9701c37daa8d077af4a3ba9415014b6e106
PureDimension <PureDimension@github.com>
2025-05-25 18:48:52 +0800

    chore: auto-update commit-log.js

--- commit-log.js
+++ commit-log.js
@@ -1,5 +1,11 @@
 export const logs = [
   {
+    "title": "\u6f6e\u6c50\u5468\u671f\u8ba1\u5212",
+    "date": "2025-05-25 18:48:30",
+    "slug": "2025-5-25-\u6f6e\u6c50\u5468\u671f\u8ba1\u5212",
+    "section": "ark"
+  },
+  {
     "title": "\u7f51\u7ad9\u914d\u7f6e\u66f4\u65b0\u65e5\u5fd7",
     "date": "2025-05-24 11:03:00",
     "slug": "2025-5-12-\u7f51\u7ad9\u914d\u7f6e\u66f4\u65b0\u65e5\u5fd7",
@@ -22,11 +28,5 @@ export const logs = [
     "date": "2025-05-21 01:09:02",
     "slug": "2025-5-21-\u5b58\u5728\u7684\u610f\u4e49",
     "section": "star-dream"
-  },
-  {
-    "title": "\u957f\u7ebfDDL\u4f5c\u6218\u8bb0\u5f55",
-    "date": "2025-05-21 01:05:30",
-    "slug": "2025-5-19-\u957f\u7ebfDDL\u4f5c\u6218\u8bb0\u5f55",
-    "section": "cloud-leaf"
   }
 ];
