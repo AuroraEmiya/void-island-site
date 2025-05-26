8dca625dcc4beaf93883b91a06f3a4a555e7d00a
PureDimension <PureDimension@github.com>
2025-05-26 21:51:25 +0800

    chore: auto-update commit-log.js

--- commit-log.js
+++ commit-log.js
@@ -1,14 +1,20 @@
 export const logs = [
   {
-    "title": "\u6f6e\u6c50\u5468\u671f\u8ba1\u5212",
-    "date": "2025-05-25 18:48:30",
-    "slug": "2025-5-25-\u6f6e\u6c50\u5468\u671f\u8ba1\u5212",
+    "title": "\u7f51\u7ad9\u914d\u7f6e\u66f4\u65b0\u65e5\u5fd7",
+    "date": "2025-05-26 21:50:01",
+    "slug": "2025-5-12-\u7f51\u7ad9\u914d\u7f6e\u66f4\u65b0\u65e5\u5fd7",
     "section": "ark"
   },
   {
-    "title": "\u7f51\u7ad9\u914d\u7f6e\u66f4\u65b0\u65e5\u5fd7",
-    "date": "2025-05-24 11:03:00",
-    "slug": "2025-5-12-\u7f51\u7ad9\u914d\u7f6e\u66f4\u65b0\u65e5\u5fd7",
+    "title": "\u7a7a\u5c9b\u9020\u7269\u4ecb\u7ecd\uff08\u5148\u5360\u5751\uff0c\u8fd8\u6ca1\u5199\u5b8c\uff09",
+    "date": "2025-05-26 21:47:09",
+    "slug": "2025-5-12-\u7a7a\u5c9b\u9020\u7269\u4ecb\u7ecd",
+    "section": "island-world"
+  },
+  {
+    "title": "\u6f6e\u6c50\u5468\u671f\u8ba1\u5212",
+    "date": "2025-05-25 18:48:30",
+    "slug": "2025-5-25-\u6f6e\u6c50\u5468\u671f\u8ba1\u5212",
     "section": "ark"
   },
   {
@@ -21,12 +27,6 @@ export const logs = [
     "title": "20250521\u68a6\u65e5\u8bb0\u7d20\u6750",
     "date": "2025-05-21 08:07:52",
     "slug": "2025-5-21-\u68a6\u65e5\u8bb0",
-    "section": "star-dream"
-  },
-  {
-    "title": "\u5b58\u5728\u7684\u610f\u4e49",
-    "date": "2025-05-21 01:09:02",
-    "slug": "2025-5-21-\u5b58\u5728\u7684\u610f\u4e49",
     "section": "star-dream"
   }
 ];
