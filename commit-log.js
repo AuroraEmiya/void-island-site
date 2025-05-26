2eec8524580471555052104432b11a6a16cdd211
PureDimension <PureDimension@github.com>
2025-05-27 00:05:55 +0800

    测试commit-log抓取逻辑

--- commit-log.js
+++ commit-log.js
@@ -1,7 +1,7 @@
 export const logs = [
   {
     "title": "\u7f51\u7ad9\u914d\u7f6e\u66f4\u65b0\u65e5\u5fd7",
-    "date": "2025-05-26 23:54:38",
+    "date": "2025-05-27 00:01:35",
     "slug": "2025-5-12-\u7f51\u7ad9\u914d\u7f6e\u66f4\u65b0\u65e5\u5fd7",
     "section": "ark"
   },
@@ -30,3 +30,24 @@ export const logs = [
     "section": "star-dream"
   }
 ];
+
+export const lastModified = {
+  "blog/ark/2025-5-12-\u7f51\u7ad9\u914d\u7f6e\u66f4\u65b0\u65e5\u5fd7.md": "2025-05-27T00:01:35.905577",
+  "blog/ark/2025-5-25-\u6f6e\u6c50\u5468\u671f\u8ba1\u5212.md": "2025-05-25T18:48:30.391648",
+  "blog/cloud-leaf/2025-5-19-\u957f\u7ebfDDL\u4f5c\u6218\u8bb0\u5f55.md": "2025-05-21T01:05:30.036208",
+  "blog/fog-radio/2025-5-10-\u7b2c\u4e09\u6b21\u5faa\u73af.md": "2025-05-11T21:42:48.264108",
+  "blog/fog-radio/2025-5-11-\u5faa\u73af\u7684\u7406\u7531.md": "2025-05-11T21:42:55.959760",
+  "blog/island-world/2025-5-10-\u6e32\u67d3\u6d4b\u8bd5.md": "2025-05-11T03:08:56.865520",
+  "blog/island-world/2025-5-12-\u7a7a\u5c9b\u9020\u7269\u4ecb\u7ecd.md": "2025-05-26T21:47:09.263071",
+  "blog/island-world/2025-5-14-\u03a9\u7a7a\u5c9b\u7684\u79d8\u5bc6.md": "2025-05-14T03:59:29.070846",
+  "blog/self-introduction/FavoriteAnimes.md": "2025-05-13T12:48:57.548650",
+  "blog/self-introduction/FavoriteGames.md": "2025-05-13T12:49:09.802085",
+  "blog/star-dream/2025-5-11-\u81ea\u7531\u7684\u610f\u4e49.md": "2025-05-11T21:34:40.363878",
+  "blog/star-dream/2025-5-14-\u68a6\u65e5\u8bb0.md": "2025-05-16T10:20:52.375788",
+  "blog/star-dream/2025-5-16-\u68a6\u65e5\u8bb0.md": "2025-05-16T10:20:24.677648",
+  "blog/star-dream/2025-5-20-\u68a6\u65e5\u8bb0.md": "2025-05-21T00:27:15.435289",
+  "blog/star-dream/2025-5-21-\u5b58\u5728\u7684\u610f\u4e49.md": "2025-05-21T01:09:02.511367",
+  "blog/star-dream/2025-5-21-\u68a6\u65e5\u8bb0.md": "2025-05-21T08:07:52.256917",
+  "blog/star-dream/2025-5-22-\u521b\u4f5c\u7684\u610f\u4e49.md": "2025-05-21T09:01:10.816519",
+  "blog/white-hall/2025-05-12-\u524d\u7aef\u5f00\u53d1\u7ecf\u9a8c.md": "2025-05-14T03:06:39.947877"
+};
