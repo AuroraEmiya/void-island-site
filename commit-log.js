be89abb659f3e2b143a3a35392495dcb92cfa6e3
PureDimension <1244288790@qq.com>
2025-07-11 18:10:46 +0800

    chore: auto-update commit-log.js

--- commit-log.js
+++ commit-log.js
@@ -1,7 +1,13 @@
 export const logs = [
   {
+    "title": "\u559c\u6b22\u7684\u6e38\u620f\u5217\u8868",
+    "date": "2025-07-11 18:09:38",
+    "slug": "FavoriteGames",
+    "section": "self-introduction"
+  },
+  {
     "title": "\u50cf\u7d20\u753b\u5b66\u4e60\u7b14\u8bb0",
-    "date": "2025-06-27 11:53:28",
+    "date": "2025-06-27 22:35:47",
     "slug": "2025-06-26-\u50cf\u7d20\u753b\u5b66\u4e60\u7b14\u8bb0",
     "section": "white-hall"
   },
@@ -22,12 +28,6 @@ export const logs = [
     "date": "2025-06-04 19:41:29",
     "slug": "2025-5-25-\u6f6e\u6c50\u5468\u671f\u8ba1\u5212",
     "section": "ark"
-  },
-  {
-    "title": "\u5f62\u7b56\u4f5c\u4e1a",
-    "date": "2025-06-04 19:37:55",
-    "slug": "2025-6-4-\u5f62\u7b56\u4f5c\u4e1a",
-    "section": "cloud-leaf"
   }
 ];
 
@@ -45,7 +45,7 @@ export const lastModified = {
   "blog/island-world/2025-5-14-\u03a9\u7a7a\u5c9b\u7684\u79d8\u5bc6.md": "2025-05-14T03:59:29.070846",
   "blog/island-world/2025-6-1-\u5929\u5883\u4ecb\u7ecd.md": "2025-06-01T22:37:24.039725",
   "blog/self-introduction/FavoriteAnimes.md": "2025-05-13T12:48:57.548650",
-  "blog/self-introduction/FavoriteGames.md": "2025-05-13T12:49:09.802085",
+  "blog/self-introduction/FavoriteGames.md": "2025-07-11T18:09:38.248330",
   "blog/star-dream/2025-5-11-\u81ea\u7531\u7684\u610f\u4e49.md": "2025-05-11T21:34:40.363878",
   "blog/star-dream/2025-5-14-\u68a6\u65e5\u8bb0.md": "2025-05-16T10:20:52.375788",
   "blog/star-dream/2025-5-16-\u68a6\u65e5\u8bb0.md": "2025-05-16T10:20:24.677648",
@@ -56,5 +56,5 @@ export const lastModified = {
   "blog/star-dream/2025-5-28-\u68a6\u65e5\u8bb0.md": "2025-05-28T12:26:25.409405",
   "blog/white-hall/2025-05-12-\u524d\u7aef\u5f00\u53d1\u7ecf\u9a8c.md": "2025-05-14T03:06:39.947877",
   "blog/white-hall/2025-06-10-\u300a\u54f2\u5b66\u5bb6\u4eec\u90fd\u5e72\u4e86\u4e9b\u4ec0\u4e48\u300b\u8bfb\u540e\u611f\u2014\u2014\u4e00\u4e2a\u7edd\u5bf9\u771f\u7406\u4e0d\u5b58\u5728\u7684\u4e16\u754c.md": "2025-06-10T13:39:37.890620",
-  "blog/white-hall/2025-06-26-\u50cf\u7d20\u753b\u5b66\u4e60\u7b14\u8bb0.md": "2025-06-27T11:53:28.043789"
+  "blog/white-hall/2025-06-26-\u50cf\u7d20\u753b\u5b66\u4e60\u7b14\u8bb0.md": "2025-06-27T22:35:47.807181"
 };
