ef53f7022b52f1c146c3a51bdd428bf5bdddbd6f
PureDimension <1244288790@qq.com>
2025-08-28 23:14:26 +0800

    fix bug

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,18 @@
+import { Suspense } from "react";
 import { getPostsBySection } from "@/lib/posts";
 import HomeContent from "./HomeContent";
 import getRecentBlogMetadata from "@/lib/RecentBlogMetadata";
 
 export default function HomePage() {
-	const selfIntroPosts = getPostsBySection("self-introduction");
-	const recentPosts = getRecentBlogMetadata();
-	return <HomeContent selfIntroPosts={selfIntroPosts} headlinePosts={recentPosts} />;
+  const selfIntroPosts = getPostsBySection("self-introduction");
+  const recentPosts = getRecentBlogMetadata();
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <HomeContent
+        selfIntroPosts={selfIntroPosts}
+        headlinePosts={recentPosts}
+      />
+    </Suspense>
+  );
 }
