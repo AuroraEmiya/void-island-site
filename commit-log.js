17741f4eea418d8a2a71071f048a9a89061c203e
PureDimension <1244288790@qq.com>
2025-09-16 23:27:07 +0800

    chore: auto-update commit-log.js

--- commit-log.js
+++ commit-log.js
@@ -1,5 +1,11 @@
 export const logs = [
   {
+    "title": "\u5982\u4f55\u4f4e\u6210\u672c\u83b7\u5f97gemini\u5b66\u751f\u8ba4\u8bc1.",
+    "date": "2025-09-16 23:26:50",
+    "slug": "2025-09-16-\u5982\u4f55\u4f4e\u6210\u672c\u83b7\u5f97gemini\u5b66\u751f\u8ba4\u8bc1",
+    "section": "white-hall"
+  },
+  {
     "title": "\u7a7a\u5c9b\u5386\u4f53\u7cfb\u8bf4\u660e",
     "date": "2025-09-12 20:46:25",
     "slug": "2025-6-1-\u7a7a\u5c9b\u5386\u4f53\u7cfb\u8bf4\u660e",
@@ -22,12 +28,6 @@ export const logs = [
     "date": "2025-09-12 19:52:21",
     "slug": "2025-7-22-\u4e50\u624d\u6d3e\u7f16\u66f2\u73ed\u5b66\u4e60\u7b14\u8bb0",
     "section": "white-hall"
-  },
-  {
-    "title": "\u7f51\u7ad9\u914d\u7f6e\u66f4\u65b0\u65e5\u5fd7",
-    "date": "2025-08-28 22:53:52",
-    "slug": "2025-5-12-\u7f51\u7ad9\u914d\u7f6e\u66f4\u65b0\u65e5\u5fd7",
-    "section": "ark"
   }
 ];
 
@@ -62,5 +62,6 @@ export const lastModified = {
   "blog/white-hall/2025-05-12-\u524d\u7aef\u5f00\u53d1\u7ecf\u9a8c.md": "2025-05-14T03:06:39.947877",
   "blog/white-hall/2025-06-10-\u300a\u54f2\u5b66\u5bb6\u4eec\u90fd\u5e72\u4e86\u4e9b\u4ec0\u4e48\u300b\u8bfb\u540e\u611f\u2014\u2014\u4e00\u4e2a\u7edd\u5bf9\u771f\u7406\u4e0d\u5b58\u5728\u7684\u4e16\u754c.md": "2025-06-10T13:39:37.890620",
   "blog/white-hall/2025-06-26-\u50cf\u7d20\u753b\u5b66\u4e60\u7b14\u8bb0.md": "2025-06-27T22:35:47.807181",
+  "blog/white-hall/2025-09-16-\u5982\u4f55\u4f4e\u6210\u672c\u83b7\u5f97gemini\u5b66\u751f\u8ba4\u8bc1.md": "2025-09-16T23:26:50.192719",
   "blog/white-hall/2025-7-22-\u4e50\u624d\u6d3e\u7f16\u66f2\u73ed\u5b66\u4e60\u7b14\u8bb0.md": "2025-09-12T19:52:21.327968"
 };
